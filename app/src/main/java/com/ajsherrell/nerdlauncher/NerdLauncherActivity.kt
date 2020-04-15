06fd8598dbfe6e7a4707f1e254496077fc0fe8cc
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-15 14:16:32 -0400

    query the packageManager for activities

--- app/src/main/java/com/ajsherrell/nerdlauncher/NerdLauncherActivity.kt
+++ app/src/main/java/com/ajsherrell/nerdlauncher/NerdLauncherActivity.kt
@@ -1,9 +1,13 @@
 package com.ajsherrell.nerdlauncher
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+
+private const val TAG = "NerdLauncherActivity"
 
 class NerdLauncherActivity : AppCompatActivity() {
 
@@ -15,5 +19,17 @@ class NerdLauncherActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.app_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        val startupIntent = Intent(Intent.ACTION_MAIN).apply {
+            addCategory(Intent.CATEGORY_LAUNCHER)
+        }
+
+        val activities = packageManager.queryIntentActivities(startupIntent, 0)
+
+        Log.i(TAG, "Found ${activities.size} activities!!!")
     }
 }
